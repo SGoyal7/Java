36dff2586a3b02823e156a026d29900ae1c17a3f
SGoyal7 <goyalshubham7@gmail.com>
2022-04-03 21:33:28 +0530

    update

---
+++ SpringBoot/main/java/com/shubham/bootjpa/AlienController.java
@@ -0,0 +1,13 @@
+package com.shubham.bootjpa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AlienController {
+    
+    @RequestMapping("/")
+    public String home() {
+        return "home.jsp";
+    }
+}
